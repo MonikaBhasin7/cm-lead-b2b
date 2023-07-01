7d52bfd0b17435df8c20285b4c9f0d996ad22b37
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2023-07-01 09:59:27 +0530

    added right image

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,22 +1,20 @@
-import './login.scss'
+import "./login.scss";
 import SignIn from "./components/signIn/signIn";
 
 function Login() {
-    return (
-        <div className='root'>
-            {/*HK*/}
-            <div className='loginContainer'>
-                <div style={{flexGrow: 1}}>
-
-                </div>
-                <SignIn/>
-                <div style={{flexGrow: 1}}></div>
-            </div>
-            <div className='carousalContainer'>
-                carousal
-            </div>
-        </div>
-    )
+  return (
+    <div className="root">
+      {/*HK*/}
+      <div className="loginContainer">
+        <div style={{ flexGrow: 1 }}></div>
+        <SignIn />
+        <div style={{ flexGrow: 1 }}></div>
+      </div>
+      <div className="carousalContainer">
+        <img src="https://images.unsplash.com/photo-1579684385127-1ef15d508118?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80" />
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
